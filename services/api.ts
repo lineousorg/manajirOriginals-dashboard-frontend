74001b9d9f52cc5283ab4dca35979f7866700ff1
Dominic <prantogomes09@gmail.com>
2026-02-19 09:49:12 +0600

    Changed api base url

--- services/api.ts
+++ services/api.ts
@@ -12,8 +12,8 @@ import {
 import { Order, OrderStatus, UpdateOrderStatusInput } from "@/types/order";
 import { User } from "@/types/user";
 
-const API_BASE_URL = "http://localhost:5000/api"; // your backend
-// const API_BASE_URL = "https://manajiroriginals.com/api";
+// const API_BASE_URL = "http://localhost:5000/api"; // your backend
+const API_BASE_URL = "https://manajiroriginals.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
