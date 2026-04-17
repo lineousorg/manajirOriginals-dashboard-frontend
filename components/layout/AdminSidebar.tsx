9ec6dc63c15570fb0c6e538bfb7b4072dbcc27ff
rezalineous-crypto <rezalineous@gamil.com>
2026-04-17 13:52:08 +0600

    final

--- components/layout/AdminSidebar.tsx
+++ components/layout/AdminSidebar.tsx
@@ -61,7 +61,7 @@ export const AdminSidebar = () => {
                 <ShoppingBag className="w-4 h-4 text-accent-foreground" />
               </div>
               <span className="font-semibold text-lg text-sidebar-foreground">
-                Commerces
+                Commerce
               </span>
             </motion.div>
           ) : (
